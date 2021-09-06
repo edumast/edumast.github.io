5dea93af75e2dbbee83dc61102b0a5896358afb0
edumast <86361043+edumast@users.noreply.github.com>
2021-09-06 12:03:20 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 alert("bem vindo a página de edumast!");
 let variavel;
 variavel=1;
-if(variavel==1) {
-alert("ola")
-				
+
+
+function ms(){
+ 
+var nome = document.getElementById("nome").value;
+var data =
+ document.getElementById("data").value;
+var senha = document.getElementById("senha").value;
+ 
+var texto ="seu nome é:"+ nome +" e sua senha é:"+ senha; 
+ 
+ 
+document.getElementById("est").innerHTML=(texto);
+ 
+ 
+return true;
+ 
 }
+ 
+
+				
+
