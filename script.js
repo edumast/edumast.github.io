fcb88dc6af3e0879dcb7cf9979a3ef0bdfe9bdd2
edumast <86361043+edumast@users.noreply.github.com>
2021-10-08 19:32:39 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 
 let variavel;
 variavel=1;
-var nome
-
-
+var nome;
+var senh;
+var data;
+var nome;
+var texto;
 function ms(){
 nome = document.getElementById("nome").value;
-var data=document.getElementById("data").value;
-var senh=document.getElementById("senh").value;
+data=document.getElementById("data").value;
+senh=document.getElementById("senh").value;
  
-var texto ="seu nome é:"+ nome +" e sua senha é:"+ senh; 
- 
- 
+texto ="seu nome é:"+ nome +" e sua senha é:"+ senh; 
 document.getElementById("est").innerHTML=(texto);
 
- 
-return true;
+ return true;
 }
-function pres(){
-	document.getElementById("txto").innerHTML=("teste");				
+function stl(){
+				document.getElementById("hh").innerHTML=(texto);
+				return true;
 }
- 
-
-				
-
+pcteste();
+function pcteste(){
+pc=true;
+				if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) { 
+				pc=false;							
+				}
+				if (pc==true){
+								alert("atenção este site não foi projetado para pc! as proporções de tela estarão erradas!")
+				}
+}
