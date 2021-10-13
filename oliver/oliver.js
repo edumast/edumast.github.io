393308333b35de7bdd84e30e17de6d49653744df
edumast <86361043+edumast@users.noreply.github.com>
2021-10-13 16:20:33 -0300

    Add files via upload

--- oliver/oliver.js
+++ oliver/oliver.js
@@ -4,6 +4,9 @@ var chli;
 var hhl;
 var bom;
 var mdc;
+var ling;
+ling="eng"; 
+var mdl=false;
 mdc=false;
 rs=" \\_(0_0)_/";
 chli=document.getElementById("chli").value;
@@ -18,11 +21,21 @@ mdc=false;
     } else if (chmy=="hello"||chmy=="hl"||chmy=="hello oliver"||chmy=="hey"||chmy=="hey oliver"||chmy=="hi"||chmy=="hi oliver"){
     rs="Hello edumast";
   }else if(chmy=="bom dia"||chmy=="good day"){
+  if (ling=="eng"){
+    rs="good day";
+  }
+  else if
+    (ling=="br"){
   rs="bom dia!";
-  bom=true;
+      
+  }  
+
   }else if(chmy=="mudar cor"){
 mdc=true;  
+}else if (mdl==true){
+    ling=chmy;
+  }else if(chmy=="mudar lingua"){
+  mdl=true;
   }
    document.getElementById("chli").innerHTML=(rs);
- console.log(rs)
 }
