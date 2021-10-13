3c0c6ed0f7685fd12d20d8499c9aa9f0a02e0e4f
edumast <86361043+edumast@users.noreply.github.com>
2021-10-13 19:55:20 -0300

    Add files via upload

--- oliver/oliver.js
+++ oliver/oliver.js
@@ -5,37 +5,59 @@ var hhl;
 var bom;
 var mdc;
 var ling;
-ling="eng"; 
+ling="br"; 
+var nome;
+nome="edumast" 
 var mdl=false;
 mdc=false;
-rs=" \\_(0_0)_/";
+rs="\\_(0_0)_/";
 chli=document.getElementById("chli").value;
+
 function env(){
   resp();
 }
 function resp(){  
 chmy=document.getElementById("chmy").value;
-  
 if (mdc==true){ document.body.style.backgroundColor = chmy;
 mdc=false;
-    } else if (chmy=="hello"||chmy=="hl"||chmy=="hello oliver"||chmy=="hey"||chmy=="hey oliver"||chmy=="hi"||chmy=="hi oliver"){
-    rs="Hello edumast";
-  }else if(chmy=="bom dia"||chmy=="good day"){
-  if (ling=="eng"){
-    rs="good day!";
+    } else if (chmy=="hello"||chmy=="hl"||chmy=="hello oliver"||chmy=="hey"||chmy=="hey oliver"||chmy=="hi"||chmy=="hi oliver"||chmy=="ola"||chmy=="oi"||chmy=="ola oliver"||chmy=="oi oliver"){
+  if (ling=="ing"){
+    rs="hello "+nome;
+  }else if (ling=="br"){
+    rs="olá "+nome;
+    }
+}else if(chmy=="bom dia"||chmy=="good day"){
+  if (ling=="ing"){
+    rs="good day "+nome+"!";
   }
   else if
     (ling=="br"){
-  rs="bom dia!";
+  rs="bom dia "+nome+"!";
       
   }  
 
-  }else if(chmy=="mudar cor"){
+  }else if(chmy=="mudar cor"||chmy=="cor"||chmy=="color"||chmy=="change color"
+          ){
 mdc=true;  
 }else if (mdl==true){
     ling=chmy;
-  }else if(chmy=="mudar lingua"){
+  if (ling=="inglês"||ling=="ingles"){
+    ling="ing";
+  }else if (ling=="portugues"||ling=="pt"||ling=="português"||ling=="br"||ling=="brasil"){
+    ling="br";
+  }
+  mdl=false;
+  }else if(chmy=="mudar idioma"||chmy=="idioma"){
   mdl=true;
   }
-   document.getElementById("chli").innerHTML=(rs);
+  
+document.getElementById("chli").innerHTML=(rs);
 }
+const inputEle = document.getElementById('enter');
+inputEle.addEventListener('keyup', function(e){
+  var key = e.which || e.keyCode;
+  if (key == 13) { 
+    document.getElementById("chli").innerHTML=(chmy);
+  }
+});
+
